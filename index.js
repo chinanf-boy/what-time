6ce466ae12440be73e5096e392cb801990b3a5fb
chinanf-boy <865501259@qq.com>
2018-06-01 11:50:44 +0800

    Done

--- index.js
+++ index.js
@@ -1,43 +1,46 @@
-var util = require('util');
-
 module.exports = human;
 
 function human(seconds,isSuffix) {
-  if (seconds instanceof Date)
-	seconds = Math.round((Date.now() - seconds) / 1000);
-
-	isSuffix && ( let suffix = seconds < 0 ? 'from now' : 'ago' )
-
+  if (seconds instanceof Date){
+	  seconds = Math.round((Date.now() - seconds) / 1000);
+  }
 
   seconds = Math.abs(seconds);
 
-  var times = [
-    seconds / 60 / 60 / 24 / 365, // years
-    seconds / 60 / 60 / 24 / 30,  // months
-    seconds / 60 / 60 / 24 / 7,   // weeks
-    seconds / 60 / 60 / 24,       // days
-    seconds / 60 / 60,            // hours
-    seconds / 60,                 // minutes
-    seconds                       // seconds
+  let names = ['Y', 'M', 'W', 'd', 'h', 'm', 's'];
+  let cales = [
+     60*60*24*365, // years
+     60*60*24*30,  // months
+     60*60*24*7,   // weeks
+     60*60*24,   // days
+     60*60,     // hours
+     60,       // minutes
+     1        // seconds
   ];
-  var names = ['year', 'month', 'week', 'day', 'hour', 'minute', 'second'];
+
+  let times = {}
+  names.forEach((name,i) =>{
+		let s = seconds / cales[i]
+		s = Math.floor(s)
+		times[name] = s
+		seconds = seconds % cales[i]
+  })
 
   let result = ''
 
-  for (var i = 0; i < names.length; i++) {
-	if(result){
-		break
-	}
-    var time = Math.floor(times[i]);
-    if (time > 1)
-      result =  util.format('%d %ss', time, names[i]);
-    else if (time === 1)
-      result = util.format('%d %s', time, names[i]);
-  }
+  Object.keys(times).forEach((t,i) =>{
+		let T = times[t]
+	  if(T){
+			result += `${T}${names[i]} `
+	  }
+  })
 
-  !result && result = util.format('0 seconds');
+  !result && (result = '0s ')
 
-  result = isSuffix ? result + ` ${suffix}` : result
+  let suffix
+  isSuffix && ( suffix = seconds < 0 ? 'from now' : 'ago' )
+
+  result = isSuffix ? result + `${suffix}` : result
 
   return result
 }
--- test.js
+++ test.js
@@ -1,11 +1,50 @@
 import test from 'ava';
-import m from '.';
+import m from './index';
 
-test('title', t => {
-	const err = t.throws(() => {
-		m(123);
-	}, TypeError);
-	t.is(err.message, 'Expected a string, got number');
+test('0', t => {
+	t.is(m(0), "0s ")
+});
 
-	t.is(m('unicorns'), 'unicorns & rainbows');
+test('0 time', t => {
+	t.is(m(0,true), "0s ago")
+});
+
+test('60', t => {
+	t.is(m(60), "1m ")
+});
+
+test('60 time', t => {
+	t.is(m(60,true), "1m ago")
+});
+
+test('1810', t => {
+	t.is(m(1810), "30m 10s ")
+});
+
+test('1810 time', t => {
+	t.is(m(1810,true), "30m 10s ago")
+});
+
+test('3600', t => {
+	t.is(m(3600), "1h ")
+});
+
+test('3600 time', t => {
+	t.is(m(3600,true), "1h ago")
+});
+
+test('1527824212 ', t => {
+	t.is(m(1527824212), "48Y 5M 1W 6d 3h 36m 52s ")
+});
+
+test('1527824212 time', t => {
+	t.is(m(1527824212,true), "48Y 5M 1W 6d 3h 36m 52s ago")
+});
+
+test('Date ', t => {
+	t.is(!!m(new Date()), true)
+});
+
+test('Date time', t => {
+	t.is(!!m(new Date(),true), true)
 });
